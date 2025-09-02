7f788687ce0f68233ae0b680a8c737af86264883
devmahant684 <mahantdev48@gmail.com>
2025-09-02 15:57:53 +0530

    change 1

--- ecs-pipeline/stacks/app_stack.py
+++ ecs-pipeline/stacks/app_stack.py
@@ -1,38 +1,76 @@
 from aws_cdk import Stack, Environment
 from constructs import Construct
-from stacks.ecs.ecs_stack import ECSFargateSimpleStack
-from stacks.codebuild.codebuild_stack import PipelineWithBuildStack
+# from stacks.ecs.ecs_stack import ECSFargateSimpleStack
+# from stacks.codebuild.codebuild_stack import PipelineWithBuildStack
+from stacks.ec2.autoscallinggroup_stack import AutoScalingGroupStack
+from stacks.codebuild.codebuild_stack import PipelineWithASGStack
 
 class AppStack(Stack):
     def __init__(self, scope: Construct, id: str, *, config: dict, env_name: str, base_stack, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        # ECS Fargate service using BaseStack resources
-        self.ecs_stack = ECSFargateSimpleStack(
+        # # ECS Fargate service using BaseStack resources
+        # self.ecs_stack = ECSFargateSimpleStack(
+        #     self,
+        #     "ECSFargateSimpleStack",
+        #     vpc=base_stack.vpc,
+        #     target_group=base_stack.target_group,
+        #     repository=base_stack.repository,
+        #     project_name=config["PROJECT_NAME"],
+        #     env_name=env_name,
+        #     desired_count=1,
+        # )
+
+        # # CodeBuild and CodePipeline stack integrated with the ECS service
+        # self.pipeline_stack = PipelineWithBuildStack(
+        #     self,
+        #     "PipelineWithBuildStack",
+        #     project_name=config["PROJECT_NAME"],
+        #     env_name=env_name,
+        #     connection_arn=config["CONNECTION_ARN"],
+        #     repo_owner=config["REPO_OWNER"],
+        #     repo_name=config["REPO_NAME"],
+        #     branch_name=config["BRANCH_NAME"],
+        #     ecs_service=self.ecs_stack.service,  # Pass the ECS Fargate service for deploy action
+        # )
+
+
+
+
+        # ASG stack using base_stack's VPC
+        self.asg_stack = AutoScalingGroupStack(
             self,
-            "ECSFargateSimpleStack",
+            "AutoScalingGroupStack",
             vpc=base_stack.vpc,
-            target_group=base_stack.target_group,
-            repository=base_stack.repository,
             project_name=config["PROJECT_NAME"],
             env_name=env_name,
-            desired_count=1,
+            instance_type="t3.micro",
+            desired_capacity=1,
+            min_capacity=1,
+            max_capacity=2,
         )
 
-        # CodeBuild and CodePipeline stack integrated with the ECS service
-        self.pipeline_stack = PipelineWithBuildStack(
+
+         # Code pipeline integrated with the ASG stack
+        self.pipeline_stack = PipelineWithASGStack(
             self,
-            "PipelineWithBuildStack",
+            "PipelineWithASGStack",
             project_name=config["PROJECT_NAME"],
             env_name=env_name,
             connection_arn=config["CONNECTION_ARN"],
             repo_owner=config["REPO_OWNER"],
             repo_name=config["REPO_NAME"],
             branch_name=config["BRANCH_NAME"],
-            ecs_service=self.ecs_stack.service,  # Pass the ECS Fargate service for deploy action
+            asg_group=self.asg_stack.asg,
         )
 
-        # Expose ECS service and cluster for external use
-        self.ecs_service = self.ecs_stack.service
-        self.cluster = self.ecs_stack.cluster
+        # # Expose ECS service and cluster for external use
+        # self.ecs_service = self.ecs_stack.service
+        # self.cluster = self.ecs_stack.cluster
+        # self.pipeline = self.pipeline_stack.pipeline
+
+
+
+          # Expose ASG and pipeline for external use
+        self.asg = self.asg_stack.asg
         self.pipeline = self.pipeline_stack.pipeline
--- ecs-pipeline/stacks/codebuild/codebuild_stack.py
+++ ecs-pipeline/stacks/codebuild/codebuild_stack.py
@@ -1,3 +1,125 @@
+# from aws_cdk import (
+#     NestedStack,
+#     aws_codepipeline as codepipeline,
+#     aws_codepipeline_actions as cp_actions,
+#     aws_codebuild as codebuild,
+#     aws_iam as iam,
+# )
+# from constructs import Construct
+
+
+# class PipelineWithBuildStack(NestedStack):
+#     def __init__(
+#         self,
+#         scope: Construct,
+#         id: str,
+#         *,
+#         project_name: str,
+#         env_name: str,
+#         connection_arn: str,
+#         repo_owner: str,
+#         repo_name: str,
+#         branch_name: str,
+#         ecs_service,     
+#         **kwargs,
+#     ):
+#         super().__init__(scope, id, **kwargs)
+
+#         # IAM Role for CodePipeline
+#         pipeline_role = iam.Role(
+#             self,
+#             "CodePipelineRole",
+#             role_name=f"{project_name}-{env_name}-codepipeline-role",
+#             assumed_by=iam.ServicePrincipal("codepipeline.amazonaws.com"),
+#         )
+
+#         # IAM Role for CodeBuild with needed permissions
+#         build_role = iam.Role(
+#             self,
+#             "CodeBuildRole",
+#             role_name=f"{project_name}-{env_name}-codebuild-role",
+#             assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com"),
+#         )
+#         build_role.add_to_policy(
+#             iam.PolicyStatement(
+#                 actions=[
+#                     "s3:GetObject",
+#                     "ecr:GetAuthorizationToken",
+#                     "ecr:BatchCheckLayerAvailability",
+#                     "ecr:BatchGetImage",
+#                     "ecr:PutImage",
+#                     "ecr:InitiateLayerUpload",
+#                     "ecr:UploadLayerPart",
+#                     "ecr:CompleteLayerUpload",
+#                     "ecs:DescribeServices",
+#                     "ecs:DescribeTaskDefinition",
+#                     "ecs:UpdateService",
+#                     "iam:PassRole",
+#                 ],
+#                 resources=["*"],
+#             )
+#         )
+
+#         # Pipeline definition
+#         self.pipeline = codepipeline.Pipeline(
+#             self,
+#             "Pipeline",
+#             pipeline_name=f"{project_name}-{env_name}-pipeline",
+#             role=pipeline_role,
+#         )
+
+#         # Artifacts
+#         self.source_output = codepipeline.Artifact(artifact_name="SourceArtifact")
+#         self.build_output = codepipeline.Artifact(artifact_name="BuildArtifact")
+
+#         # Source stage - GitHub via CodeStar connection
+#         source_action = cp_actions.CodeStarConnectionsSourceAction(
+#             action_name="GitHub_Source",
+#             connection_arn=connection_arn,
+#             owner=repo_owner,
+#             repo=repo_name,
+#             branch=branch_name,
+#             output=self.source_output,
+#             trigger_on_push=True,
+#         )
+#         self.pipeline.add_stage(stage_name="Source", actions=[source_action])
+
+#         # Build project (buildspec should output imagedefinitions.json only)
+#         self.build_project = codebuild.PipelineProject(
+#             self,
+#             "BuildProject",
+#             project_name=f"{project_name}-{env_name}-codebuild",
+#             build_spec=codebuild.BuildSpec.from_source_filename("ecs-pipeline/buildspec.yaml"),
+#             environment=codebuild.BuildEnvironment(
+#                 build_image=codebuild.LinuxBuildImage.STANDARD_7_0,
+#                 privileged=True,
+#             ),
+#             role=build_role,
+#         )
+
+#         # Build action
+#         self.build_action = cp_actions.CodeBuildAction(
+#             action_name="Build",
+#             project=self.build_project,
+#             input=self.source_output,
+#             outputs=[self.build_output],
+#         )
+#         self.pipeline.add_stage(stage_name="Build", actions=[self.build_action])
+
+#         # Deploy Stage using EcsDeployAction
+#         deploy_action = cp_actions.EcsDeployAction(
+#             action_name="Deploy",
+#             service=ecs_service,
+#             input=self.build_output,  # imagedefinitions.json
+#         )
+#         self.pipeline.add_stage(stage_name="Deploy", actions=[deploy_action])
+
+
+
+
+
+
+
 from aws_cdk import (
     NestedStack,
     aws_codepipeline as codepipeline,
@@ -7,8 +129,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-
-class PipelineWithBuildStack(NestedStack):
+class PipelineWithASGStack(NestedStack):
     def __init__(
         self,
         scope: Construct,
@@ -20,12 +141,11 @@ class PipelineWithBuildStack(NestedStack):
         repo_owner: str,
         repo_name: str,
         branch_name: str,
-        ecs_service,     
+        asg_group,  # AutoScalingGroup instance
         **kwargs,
     ):
         super().__init__(scope, id, **kwargs)
 
-        # IAM Role for CodePipeline
         pipeline_role = iam.Role(
             self,
             "CodePipelineRole",
@@ -33,7 +153,6 @@ class PipelineWithBuildStack(NestedStack):
             assumed_by=iam.ServicePrincipal("codepipeline.amazonaws.com"),
         )
 
-        # IAM Role for CodeBuild with needed permissions
         build_role = iam.Role(
             self,
             "CodeBuildRole",
@@ -43,24 +162,15 @@ class PipelineWithBuildStack(NestedStack):
         build_role.add_to_policy(
             iam.PolicyStatement(
                 actions=[
-                    "s3:GetObject",
-                    "ecr:GetAuthorizationToken",
-                    "ecr:BatchCheckLayerAvailability",
-                    "ecr:BatchGetImage",
-                    "ecr:PutImage",
-                    "ecr:InitiateLayerUpload",
-                    "ecr:UploadLayerPart",
-                    "ecr:CompleteLayerUpload",
-                    "ecs:DescribeServices",
-                    "ecs:DescribeTaskDefinition",
-                    "ecs:UpdateService",
+                    "autoscaling:UpdateAutoScalingGroup",
+                    "ec2:CreateImage",
+                    "ec2:DescribeInstances",
                     "iam:PassRole",
                 ],
                 resources=["*"],
             )
         )
 
-        # Pipeline definition
         self.pipeline = codepipeline.Pipeline(
             self,
             "Pipeline",
@@ -68,11 +178,10 @@ class PipelineWithBuildStack(NestedStack):
             role=pipeline_role,
         )
 
-        # Artifacts
         self.source_output = codepipeline.Artifact(artifact_name="SourceArtifact")
         self.build_output = codepipeline.Artifact(artifact_name="BuildArtifact")
 
-        # Source stage - GitHub via CodeStar connection
+        # Source stage: GitHub repo via CodeStar connection
         source_action = cp_actions.CodeStarConnectionsSourceAction(
             action_name="GitHub_Source",
             connection_arn=connection_arn,
@@ -84,7 +193,7 @@ class PipelineWithBuildStack(NestedStack):
         )
         self.pipeline.add_stage(stage_name="Source", actions=[source_action])
 
-        # Build project (buildspec should output imagedefinitions.json only)
+        # Build stage referencing ecs-pipeline/buildspec.yaml
         self.build_project = codebuild.PipelineProject(
             self,
             "BuildProject",
@@ -92,24 +201,33 @@ class PipelineWithBuildStack(NestedStack):
             build_spec=codebuild.BuildSpec.from_source_filename("ecs-pipeline/buildspec.yaml"),
             environment=codebuild.BuildEnvironment(
                 build_image=codebuild.LinuxBuildImage.STANDARD_7_0,
-                privileged=True,
+                privileged=False,
             ),
             role=build_role,
         )
-
-        # Build action
-        self.build_action = cp_actions.CodeBuildAction(
+        build_action = cp_actions.CodeBuildAction(
             action_name="Build",
             project=self.build_project,
             input=self.source_output,
             outputs=[self.build_output],
         )
-        self.pipeline.add_stage(stage_name="Build", actions=[self.build_action])
+        self.pipeline.add_stage(stage_name="Build", actions=[build_action])
 
-        # Deploy Stage using EcsDeployAction
-        deploy_action = cp_actions.EcsDeployAction(
+        # Deploy stage referencing ecs-pipeline/buildspec-deploy.yaml
+        self.deploy_project = codebuild.PipelineProject(
+            self,
+            "DeployProject",
+            project_name=f"{project_name}-{env_name}-deploy",
+            build_spec=codebuild.BuildSpec.from_source_filename("ecs-pipeline/buildspec-deploy.yaml"),
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_7_0,
+            ),
+            role=build_role,
+        )
+        deploy_action = cp_actions.CodeBuildAction(
             action_name="Deploy",
-            service=ecs_service,
-            input=self.build_output,  # imagedefinitions.json
+            project=self.deploy_project,
+            input=self.build_output,
         )
         self.pipeline.add_stage(stage_name="Deploy", actions=[deploy_action])
+
